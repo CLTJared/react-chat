7f2b0512f4d02df9ab3a461c31f1fc936a0c50b0
Jared Elliott <bootcamp@jaredclt.com>
2024-03-29 14:05:20 -0400

    .

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/api', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } 

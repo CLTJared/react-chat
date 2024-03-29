92fe96b4e62b1e9a1f4598c74e89b7ffd3cb35e7
Jared Elliott <bootcamp@jaredclt.com>
2024-03-29 18:14:01 -0400

    Added notification sounds.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [chatUsers, setChatUsers] = useState([]);
   const whoAmI = socket.id;
   const chatMessage = useRef();
+  const NewUserCheck = useRef();
+  const NewMsgCheck = useRef();
 
   const connectChat = () => {
     socket.connect()
@@ -21,6 +23,9 @@ function App() {
   const handleNewChat = (e) => {
     e.preventDefault();
 
+    //keep user from submitting blank message
+    if(chatMessage.current.value.trim() === '') return 0;
+
     const d = new Date()
 
     socket.emit('messages', {
@@ -33,17 +38,34 @@ function App() {
     e.target.reset();
   }
 
+const newSound = (type, freq, gain, len) => {
+  // one context per document
+  var context = new (window.AudioContext || window.webkitAudioContext)();
+  var osc = context.createOscillator(); // instantiate an oscillator
+  var vol = context.createGain();
+  osc.type = type; // this is the default - also square, sawtooth, triangle
+  osc.frequency.value = freq; // Hz
+  vol.gain.value = gain;
+
+  osc.connect(vol); // set the volume
+  vol.connect(context.destination) // connect it to the destination
+  osc.start(); // start the oscillator
+  osc.stop(context.currentTime + len);
+}
+
   useEffect(() => {
     setChatConnected(socket.connected)
   })
 
   useEffect(() => {
     socket.on('msgResponse', (data) => setChatMessage([...chatMessages, data]))
+    if(NewMsgCheck.current.checked) newSound('triangle', 510, .005, .25)
   }, [socket, chatMessages])
 
   useEffect(() => {
     socket.on('userList', (data) => setChatUsers(data))
-  }, [])
+    if(NewUserCheck.current.checked) newSound('sine', 935, .02, .25)
+  }, [chatUsers])
 
   return (
     <>
@@ -60,6 +82,14 @@ function App() {
           <div className="__chat_info">
             {chatUsers.map((data, index) => <p name="user" key={index}>{data.id}</p>)}
           </div>
+          <div>
+            <hr />
+            <div id="__chat_options">
+              <h5>Notifications</h5>
+              <label htmlFor="NewMsg"><input ref={NewMsgCheck} type="checkbox" id="NewMsg" defaultChecked />New Message</label>
+              <label htmlFor="NewUser"><input ref={NewUserCheck} type="checkbox" id="NewUser" defaultChecked />New User</label>
+            </div>
+          </div>
         </div>
       </nav>
       {/* End Nav Component */}
@@ -72,7 +102,7 @@ function App() {
           </form>
         </div>
         <br />
-        <div>
+        <div style={{paddingTop: '.5rem' }}>
           {chatMessages.map((chat, index) => chat.socketID === whoAmI ? <div name="chat-bubble" key={index} className="out"><span className="username">{chat.socketID} | {chat.time}</span>{chat.message}</div> : <div name="chat-bubble" key={index} className="in"><span className="username">{chat.socketID} | {chat.time}</span>{chat.message}</div>)}
         </div>
       </section>
